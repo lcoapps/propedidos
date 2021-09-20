6ffc3bf3d54cd73776537d535be16f077a90685c
lcoapps <90422348+lcoapps@users.noreply.github.com>
2021-09-20 17:09:29 -0500

    Add files via upload

---
+++ Datos/Conexion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Datos
+{
+    public class Conexion
+    {
+        public static string cadena = "Server=SRV-ORION;initial catalog=UnoEE;persist security info=True;user id=sa;password=*0carpasikl1*";
+        public static string cadena2 = "Server=SRV-ORION;initial catalog=UnoEe;persist security info=True;user id=sa;password=*0carpasikl1*";
+
+    }
+}
---
+++ Datos/conexion_2020.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class conexion_2020
+    {
+        public static string cadena = "Server=SRV-ORION;initial catalog=UnoEE;persist security info=True;user id=sa;password=*0carpasikl1*";
+    }
+}
